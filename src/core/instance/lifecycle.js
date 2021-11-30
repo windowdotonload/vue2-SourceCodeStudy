7ff36da5f94cd6d7189ef814716fece523d0d3a7
windowdotonload <275981471@qq.com>
2021-11-30 22:53:54 +0800

    mod

--- src/core/instance/lifecycle.js
+++ src/core/instance/lifecycle.js
@@ -3,14 +3,40 @@
  * @version:
  * @Author: windowdotonload
  */
+/*
+ * @Descripttion:
+ * @version:
+ * @Author: windowdotonload
+ */
+import Watcher from "../observer/watcher";
 export function lifecycleMixin(Vue) {
-  Vue.prototype._update = function (vnode, hydrating) {};
+  Vue.prototype._update = function (vnode, hydrating) {
+    console.log("this is _update =>", vnode);
+  };
 }
 
 export function mountComponent(vm, el, hydrating) {
   vm.$el = el;
   let updateComponent;
+  vm._render = function () {
+    console.log("this is render in _render ");
+    return { vnode: "vnode" };
+  };
   updateComponent = () => {
     vm._update(vm._render(), hydrating);
   };
+  new Watcher(
+    vm,
+    updateComponent,
+    noop,
+    {
+      //   before() {
+      //     if (vm._isMounted && !vm._isDestroyed) {
+      //       callHook(vm, "beforeUpdate");
+      //     }
+      //   },
+    },
+    true /* isRenderWatcher */
+  );
+  return vm;
 }
---
+++ src/core/ovserver/watcher.js
@@ -0,0 +1,11 @@
+/*
+ * @Descripttion:
+ * @version:
+ * @Author: windowdotonload
+ */
+
+export default class Watcher {
+  constructor(vm, expOrFn, cb, options, isRenderWatcher) {
+    console.log("this is Watcher ===>", vm, expOrFn);
+  }
+}
