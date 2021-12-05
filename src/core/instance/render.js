2a162771e646063f4961335b23c45e1a68c7d5cd
windowdotonload <275981471@qq.com>
2021-12-05 22:11:29 +0800

    mod

--- src/core/instance/render.js
+++ src/core/instance/render.js
@@ -11,6 +11,7 @@
 export function renderMixin(Vue) {
   console.log("this is Vue in Render");
   Vue.prototype._render = function () {
+    const vm = this;
     let vnode = "vnode";
     const { render } = vm.$options;
     vnode = render.call(vm._renderProxy, vm.$createElement);
