6135b82a71da8fc0cbbf030d108808d26eea965c
windowdotonload <275981471@qq.com>
2021-12-15 22:10:17 +0800

    MOD

--- src/core/vdom/patch.js
+++ src/core/vdom/patch.js
@@ -3,8 +3,14 @@
  * @version:
  * @Author: windowdotonload
  */
+/*
+ * @Descripttion:
+ * @version:
+ * @Author: windowdotonload
+ */
 export function createPatchFunction() {
   return function patch(oldVnode, vnode, hydrating, removeOnly) {
+    // TODO
     console.log("this is createPatchFunction===========");
   };
 }
