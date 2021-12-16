83b588fd5c166bf37337b75bf7217461617e21e5
windowdotonload <275981471@qq.com>
2021-12-16 22:56:49 +0800

    mod

--- src/core/vdom/patch.js
+++ src/core/vdom/patch.js
@@ -8,9 +8,30 @@
  * @version:
  * @Author: windowdotonload
  */
+
+import { isUndef } from "../../shared/util";
+
 export function createPatchFunction() {
+  function createElm(
+    vnode,
+    insertedVnodeQueue,
+    parentElm,
+    refElm,
+    nested,
+    ownerArray,
+    index
+  ) {
+    console.log("this is createElm in createPathcFunction", vnode);
+  }
+
   return function patch(oldVnode, vnode, hydrating, removeOnly) {
     // TODO
     console.log("this is createPatchFunction===========");
+
+    if (isUndef(oldVnode)) {
+    } else {
+      const isRealElement = true;
+      createElm(vnode);
+    }
   };
 }
--- src/shared/util.js
+++ src/shared/util.js
@@ -8,4 +8,13 @@
  * @version:
  * @Author: windowdotonload
  */
+/*
+ * @Descripttion:
+ * @version:
+ * @Author: windowdotonload
+ */
 export function noop(a, b, c) {}
+
+export function isUndef(v) {
+  return v === undefined || v === null;
+}
