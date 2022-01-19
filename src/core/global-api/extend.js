fb9172c4b2f504a157c575fa7484c309467a8fc7
windowdotonload <275981471@qq.com>
2022-01-19 22:28:36 +0800

    mod

--- src/core/global-api/extend.js
+++ src/core/global-api/extend.js
@@ -1,1 +1,20 @@
-export function initExtend(Vue) {}
+import { mergeOptions } from "../util/index";
+export function initExtend(Vue) {
+  extendOptions = extendOptions || {};
+  Vue.extend = function (extendOptions) {
+    const Super = this;
+    // const SuperId = Super.cid;
+    // const cachedCtors = extendOptions._Ctor || (extendOptions._Ctor = {});
+    // if (cachedCtors[SuperId]) {
+    //   return cachedCtors[SuperId];
+    // }
+    const Sub = function VueComponent(options) {
+      this._init(options);
+    };
+    Sub.prototype = Object.create(Super.prototype);
+    Sub.prototype.constructor = Sub;
+
+    Sub.options = mergeOptions(Super.options, extendOptions);
+    return Sub;
+  };
+}
