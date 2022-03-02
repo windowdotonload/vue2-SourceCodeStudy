a2d22ce361f268b32fd7bb225d8e1a926a7b2760
windowdotonload <275981471@qq.com>
2022-03-02 20:58:19 +0800

    mod

--- src/core/vdom/patch.js
+++ src/core/vdom/patch.js
@@ -99,9 +99,6 @@ export function createPatchFunction(backend) {
         oldVnode = emptyNodeAt(oldVnode);
       }
       const oldElm = oldVnode.elm;
-      console.log("this is 【oldElm】 in patch of core/vom", oldVnode);
-      console.log("this is 【VNODE】 in patch of core/vom", vnode);
-
       const parentElm = nodeOps.parentNode(oldElm);
       console.log("this is parentElm in patch of core/vom", parentElm);
       createElm(vnode, insertedVnodeQueue, parentElm);
