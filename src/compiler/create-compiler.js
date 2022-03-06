31550e11e39874f3e3c7ded3a7139fe256b040a6
windowdotonload <275981471@qq.com>
2022-03-06 19:40:24 +0800

    mod

--- src/compiler/create-compiler.js
+++ src/compiler/create-compiler.js
@@ -1,1 +1,9 @@
-export function createCompilerCreator(baseCompile) {}
+import { createCompileToFunctionFn } from "./to-function";
+
+export function createCompilerCreator(baseCompile) {
+  return function createCompiler(baseOptions) {
+    function compile(template, options) {}
+
+    return { compile, compileToFunctions: createCompileToFunctionFn(compile) };
+  };
+}
---
+++ src/compiler/to-function.js
@@ -0,0 +1,5 @@
+export function createCompileToFunctionFn(compile) {
+  return function compileToFunctions(template, options, vm) {
+    console.log("this is compileToFunctions in compileToFunctions=====>");
+  };
+}
