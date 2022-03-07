ea9a85cd49399d095d23cc40f474b730164de92d
windowdotonload <275981471@qq.com>
2022-03-07 23:14:09 +0800

    mod

--- src/compiler/create-compiler.js
+++ src/compiler/create-compiler.js
@@ -4,6 +4,8 @@ export function createCompilerCreator(baseCompile) {
   return function createCompiler(baseOptions) {
     function compile(template, options) {
       console.log("this is compile in createCompiler", template, options);
+      const compiled = {};
+      return compiled;
     }
 
     return { compile, compileToFunctions: createCompileToFunctionFn(compile) };
