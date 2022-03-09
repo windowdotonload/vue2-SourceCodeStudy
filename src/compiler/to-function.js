638ff0e82d0020674669d297eecceb8bb9cb237a
windowdotonload <275981471@qq.com>
2022-03-09 22:55:45 +0800

    mod

--- src/compiler/to-function.js
+++ src/compiler/to-function.js
@@ -1,3 +1,12 @@
+function createFunction(code, errors) {
+  try {
+    return new Function(code);
+  } catch (err) {
+    errors.push({ err, code });
+    return noop;
+  }
+}
+
 export function createCompileToFunctionFn(compile) {
   return function compileToFunctions(template, options, vm) {
     let res = {};
