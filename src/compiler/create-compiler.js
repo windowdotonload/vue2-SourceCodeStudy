ba9381dc41fccb1bf45c196c31771744a46c6d2a
windowdotonload <275981471@qq.com>
2022-03-09 23:21:28 +0800

    EP41-09.16

--- src/compiler/create-compiler.js
+++ src/compiler/create-compiler.js
@@ -4,7 +4,7 @@ export function createCompilerCreator(baseCompile) {
   return function createCompiler(baseOptions) {
     function compile(template, options) {
       console.log("this is compile in createCompiler", template, options);
-
+      const finalOptions = Object.create(baseOptions);
       const compiled = baseCompile(template.trim(), finalOptions);
       return compiled;
     }
--- src/compiler/index.js
+++ src/compiler/index.js
@@ -4,15 +4,15 @@ export const createCompiler = createCompilerCreator(function baseCompile(
   template,
   options
 ) {
-  //   const ast = parse(template.trim(), options);
+  const ast = parse(template.trim(), options);
   //   if (options.optimize !== false) {
   //     optimize(ast, options);
   //   }
-  //   const code = generate(ast, options);
-  //   return {
-  //     ast,
-  //     render: code.render,
-  //     staticRenderFns: code.staticRenderFns,
-  //   };
+  const code = generate(ast, options);
+  return {
+    ast,
+    render: code.render,
+    staticRenderFns: code.staticRenderFns,
+  };
   return {};
 });
