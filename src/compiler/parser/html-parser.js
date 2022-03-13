7ff4e273357e16308be591123113ef8069c75f0f
windowdotonload <275981471@qq.com>
2022-03-13 14:23:34 +0800

    mod

--- src/compiler/parser/html-parser.js
+++ src/compiler/parser/html-parser.js
@@ -1,1 +1,12 @@
-export function parseHTML(html, options) {}
+import { makeMap, no } from "shared/util";
+
+export const isPlainTextElement = makeMap("script,style,textarea", true);
+
+export function parseHTML(html, options) {
+  let last, lastTag;
+  while (html) {
+    last = html;
+    if (!lastTag || !isPlainTextElement(lastTag)) {
+    }
+  }
+}
--- src/shared/util.js
+++ src/shared/util.js
@@ -35,3 +35,5 @@ const hasOwnProperty = Object.prototype.hasOwnProperty;
 export function hasOwn(obj, key) {
   return hasOwnProperty.call(obj, key);
 }
+
+export function makeMap(str, expectsLowerCase) {}
