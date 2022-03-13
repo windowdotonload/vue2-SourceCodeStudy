1447492fca7a6ee55a3e747f5d392a54f4489814
windowdotonload <275981471@qq.com>
2022-03-13 14:18:02 +0800

    mod

--- src/compiler/parser/index.js
+++ src/compiler/parser/index.js
@@ -1,9 +1,11 @@
+import { parseHTML } from "./html-parser";
 export function parse(template, options) {
   let root;
   parseHTML(template, {
     start(tag, attrs, unary, start, end) {},
     end(tag, start, end) {},
     chars(text, start, end) {},
+    comment(text, start, end) {},
   });
   return root;
 }
