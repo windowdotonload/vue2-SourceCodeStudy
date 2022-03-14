2aaf970e22da9fee8ae4ca92f1436f008bfde830
windowdotonload <275981471@qq.com>
2022-03-14 23:30:38 +0800

    mod

--- src/compiler/parser/html-parser.js
+++ src/compiler/parser/html-parser.js
@@ -3,6 +3,7 @@ import { makeMap, no } from "../../shared/util";
 export const isPlainTextElement = makeMap("script,style,textarea", true);
 
 export function parseHTML(html, options) {
+  const stack = [];
   let last, lastTag;
   while (html) {
     last = html;
